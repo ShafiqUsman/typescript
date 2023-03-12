8c0908f95e0034a596c0710fa04d8d596a32ca11
Muhammad Shafiq <98705902+ShafiqUsman@users.noreply.github.com>
2023-03-13 01:02:31 +0500

    Create grade.ts

---
+++ Assignments/classAssignments/grade.ts
@@ -0,0 +1,24 @@
+
+// Task 1: Calculate Students Grade
+
+function grade(marks1:number, marks2:number) {
+    let avg = (marks1 + marks2) / 2
+    if (avg >= 80) {
+        console.log(`you Got grade: "A"`);        
+    } else if (avg >=70) {
+        console.log(`you Got grade: "B"`);
+    } else if (avg >= 60) {
+        console.log(`you Got grade: "C"`);
+    } else if (avg >= 50) {
+        console.log(`you Got grade: "D"`);
+    } else if (avg >= 40) {
+        console.log(`you Got grade: "E"`);
+    } else if (avg >= 30) {
+        console.log(`you Got grade: "F"`);
+    } else 
+    console.log(`you Fail"`);    1
+}
+
+grade(70,90)
+grade(50,60)
+grade(10,10)
