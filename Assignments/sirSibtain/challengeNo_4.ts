b70894c82f7ed1fa71d4e9bcabd4af594d41ad47
Muhammad Shafiq <98705902+ShafiqUsman@users.noreply.github.com>
2023-03-14 23:01:59 +0500

    Create challengeNo_4.ts

---
+++ Assignments/sirSibtain/challengeNo_4.ts
@@ -0,0 +1,10 @@
+// Sir Sibtain Assignments
+// Challenge # 5
+// Generate Random Number in a given range (inclusive of both end & start limits)
+
+function generateRandomNumber(start:number, end:number):number {
+  let multiplier = end - start +1
+  return Math.floor(Math.random() * multiplier) + start
+}
+
+console.log(generateRandomNumber(3, 9))
